bffca820f5118d52f2aa060f4cf932d375f53a79
Juliana <jprado.contact@gmail.com>
2026-04-11 14:33:53 +0400

    update db config for production

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,11 +1,18 @@
 import { Pool } from 'pg'
 
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-})
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false }
+      }
+    : {
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        database: process.env.DB_NAME,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+      }
+)
 
 export default pool
